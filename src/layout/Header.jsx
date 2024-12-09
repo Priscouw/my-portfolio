28e52981a5cf6ad9938593b1975d330a8626673c
Priscouw <priscouw@gmail.com>
2024-12-09 16:49:12 +0100

    <feat> create header element

---
+++ src/layout/Header.jsx
@@ -0,0 +1,14 @@
+import Navigation from "./navigation";
+
+const Header = () => {
+  return (
+    <header className="flex-col-row border-b sticky sm:justify-between sm:border-solid sm:">
+      <h1 className="text-purple text-3xl text-center py-6 sm:px-6">
+        Priscilla
+      </h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
