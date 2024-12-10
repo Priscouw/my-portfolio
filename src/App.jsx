dea1838c2386c8a3bac9436c007d49f43be55cce
Priscouw <priscouw@gmail.com>
2024-12-10 14:23:09 +0100

    <feat> add heroheader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./layout/Header";
+import HeroHeader from "./layout/HeroHeader";
 
 function App() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <HeroHeader />
+      </main>
     </>
   );
 }
---
+++ src/layout/HeroHeader.jsx
@@ -0,0 +1,16 @@
+const HeroHeader = () => {
+  return (
+    <section className="bg-hero-pattern bg-cover bg-center h-[350px] w-screen sm:h-[450px] md:h-[550px] lg:h-[650px]">
+      <div className="flex flex-col justify-center items-center w-full h-full gap-20 sm:gap-10 sm:items-end sm:pr-10">
+        <h1 className="text-4xl font-bold text-violet-600 md:text-5xl lg:text-6xl ">
+          Priscilla
+        </h1>
+        <p className=" text-3xl font-bold text-center text-white md:text-4xl lg:text-5xl">
+          Developpeuse Frontend
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroHeader;
