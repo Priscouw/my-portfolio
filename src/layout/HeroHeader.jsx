1e6d52fb5680eed3aaf71e46da0b21a6b1a0ff30
Priscouw <priscouw@gmail.com>
2024-12-15 13:35:37 +0100

    <style> change width of heroheader

--- src/layout/HeroHeader.jsx
+++ src/layout/HeroHeader.jsx
@@ -1,7 +1,7 @@
 const HeroHeader = () => {
   return (
-    <section className="bg-hero-pattern bg-cover bg-center h-[350px] w-screen sm:h-[450px] md:h-[550px] lg:h-[650px]">
-      <div className="flex flex-col justify-center items-center w-full h-full gap-20 sm:gap-10 sm:items-end sm:pr-10">
+    <section className="bg-hero-pattern bg-cover bg-center h-[350px] max-w-full sm:h-[450px]  md:h-[550px] lg:h-[650px]">
+      <div className="flex flex-col justify-center items-center w-full  h-full gap-20 sm:gap-10 sm:items-end sm:pr-10">
         <h1 className="text-4xl font-bold text-violet-600 md:text-5xl lg:text-6xl ">
           Priscilla
         </h1>
