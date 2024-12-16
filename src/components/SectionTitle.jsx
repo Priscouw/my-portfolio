cff57b97e126ab9ee6ce4ae1d813957cb8371929
Priscouw <priscouw@gmail.com>
2024-12-16 15:46:12 +0100

    <feat> add section title component

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,13 @@
+import { PropTypes } from "prop-types";
+
+export const SectionTitle = (props) => {
+  return (
+    <h2 className="text-violet-600 text-center text-2xl mb-16 sm:text-3xl lg:text-left">
+      {props.title}
+    </h2>
+  );
+};
+
+SectionTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+};
