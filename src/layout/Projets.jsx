90b1ef4a646a49856969355848b2a958c736fc04
Priscouw <priscouw@gmail.com>
2024-12-17 10:49:41 +0100

    <style> change justify on lg and xl device

--- src/layout/Projets.jsx
+++ src/layout/Projets.jsx
@@ -3,10 +3,10 @@ import projects from "./../data/projects.json";
 
 export const Projets = () => {
   return (
-    <section className="w-[90%] max-w-6xl m-auto mt-16 mb-16" id="projets">
+    <section className="w-[90%] max-w-6xl m-auto mt-16 mb-16" id="projects">
       <SectionTitle title="Mes projets" />
 
-      <div className="flex flex-wrap gap-10 justify-center xl:justify-start">
+      <div className="flex flex-wrap gap-10 justify-center lg:justify-around xl:justify-between">
         {projects.map((projet) => (
           <button key={projet.id} className="max-w-xs">
             <img src={projet.picture} alt={projet.altPicture} />
