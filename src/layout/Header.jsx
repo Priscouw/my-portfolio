4177dbe9b5ad060f5484a62173bafacb185f6d2d
Priscouw <priscouw@gmail.com>
2024-12-17 11:59:34 +0100

    <style> add z-index

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,7 @@ import Navigation from "./navigation";
 
 const Header = () => {
   return (
-    <header className="flex-col-row border-b sticky top-0 bg-black sm:justify-between sm:border-solid sm:">
+    <header className="flex-col-row border-b sticky top-0 z-50 bg-black sm:justify-between sm:border-solid sm:">
       <h1 className="text-purple text-3xl text-center py-6 sm:px-6">
         Priscilla
       </h1>
