94dbdb4662bf0ed7aa344871622730a9976467e3
Priscouw <priscouw@gmail.com>
2024-12-17 12:55:06 +0100

    <style> add animation on projects

--- src/layout/Projets.jsx
+++ src/layout/Projets.jsx
@@ -8,8 +8,16 @@ export const Projets = () => {
 
       <div className="flex flex-wrap gap-10 justify-center lg:justify-around xl:justify-between">
         {projects.map((projet) => (
-          <button key={projet.id} className="max-w-xs">
-            <img src={projet.picture} alt={projet.altPicture} />
+          <button
+            key={projet.id}
+            className="max-w-xs transition hover:scale-110 hover:transition hover:ease-linear "
+            aria-label={`Voir le projet ${projet.name}`}
+          >
+            <img
+              src={projet.picture}
+              alt={projet.altPicture}
+              className="object-cover w-full rounded-lg"
+            />
           </button>
         ))}
       </div>
