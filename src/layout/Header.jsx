679863d4c4359cb022595e3bdaba96b469055f52
Priscouw <priscouw@gmail.com>
2024-12-20 13:51:13 +0100

    <refactor> add menu nav view mobile

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,28 @@
 import Navigation from "./navigation";
+import { LuAlignJustify } from "react-icons/lu";
+import { useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <header className="flex-col-row border-b sticky top-0 z-50 bg-black sm:justify-between sm:border-solid sm:">
-      <h1 className="text-purple text-3xl text-center py-6 sm:px-6">
-        Priscilla
-      </h1>
-      <Navigation />
+    <header className="flex-col-row border-b py-5 sticky top-0 z-50 bg-black sm:justify-between sm:border-solid">
+      <div className="flex justify-between items-center w-full px-6 sm:px-0 sm:w-auto">
+        <h1 className="text-purple text-3xl text-center sm:px-6 sm:w-auto">
+          Priscilla
+        </h1>
+        <button
+          className="sm:hidden text-white text-2xl"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <LuAlignJustify />
+        </button>
+      </div>
+      <Navigation
+        className={`h-0 overflow-hidden sm:h-auto ${
+          isMenuOpen ? "h-auto" : ""
+        }`}
+      />
     </header>
   );
 };
--- src/layout/Navigation.jsx
+++ src/layout/Navigation.jsx
@@ -1,14 +1,15 @@
-const Navigation = () => {
-  const navElements = [
-    { title: "Qui suis je ?", id: "about" },
-    { title: "Projets", id: "projects" },
-    { title: "Compétences", id: "skills" },
-    { title: "Parcours", id: "timeline" },
-    { title: "Contact", id: "contact" },
-  ];
+import propTypes from "prop-types";
+const navElements = [
+  { title: "Qui suis je ?", id: "about" },
+  { title: "Projets", id: "projects" },
+  { title: "Compétences", id: "skills" },
+  { title: "Parcours", id: "timeline" },
+  { title: "Contact", id: "contact" },
+];
 
+const Navigation = ({ className = "" }) => {
   return (
-    <nav className="flex sm:px-7">
+    <nav className={`flex justify-center sm:pr-7 ${className}`}>
       <ul className="flex-col-row gap-4 pb-5 text-white sm:text-start sm:gap-6 sm:pb-0 md:gap-8 lg:gap-10">
         {navElements.map((navElement) => (
           <li
@@ -24,3 +25,7 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
+Navigation.propTypes = {
+  className: propTypes.string,
+};
