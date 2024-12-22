08f1c72a426aabaf295f615f55089ec66c116f04
Priscouw <priscouw@gmail.com>
2024-12-22 13:10:32 +0100

    <feat> create techIcon component

---
+++ src/components/TechIcon.jsx
@@ -0,0 +1,19 @@
+import propTypes from "prop-types";
+
+// composant qui affiche le langage ou framework utilisé
+
+const TechIcon = ({ className = "", src = "", alt = "" }) => {
+  return (
+    <div className={`rounded-full overflow-hidden bg-white ${className}`}>
+      <img src={src} alt={alt} className="object-contain w-full h-full" />
+    </div>
+  );
+};
+
+export default TechIcon;
+
+TechIcon.propTypes = {
+  className: propTypes.string,
+  src: propTypes.string.isRequired,
+  alt: propTypes.string.isRequired,
+};
