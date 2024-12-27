b02699311c8e2677719f389b4093ae628aa0db85
Priscouw <priscouw@gmail.com>
2024-12-27 13:08:34 +0100

    <refactor> change props

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,13 +1,16 @@
 import { PropTypes } from "prop-types";
 
-export const SectionTitle = (props) => {
+export const SectionTitle = ({ title, className }) => {
   return (
-    <h3 className="text-purple text-center text-2xl mb-16 sm:text-3xl lg:text-left">
-      {props.title}
+    <h3
+      className={`text-purple text-center text-2xl mb-16 sm:text-3xl lg:text-left ${className}`}
+    >
+      {title}
     </h3>
   );
 };
 
 SectionTitle.propTypes = {
   title: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
