e105557888ae4673fc5874457cfa8981d57b161d
Priscouw <priscouw@gmail.com>
2024-12-27 13:40:29 +0100

    <style> add role and aria-label on navigation

--- src/layout/Navigation.jsx
+++ src/layout/Navigation.jsx
@@ -9,7 +9,11 @@ const navElements = [
 
 const Navigation = ({ className = "" }) => {
   return (
-    <nav className={`flex justify-center sm:pr-7 ${className}`}>
+    <nav
+      className={`flex justify-center sm:pr-7 ${className}`}
+      role="navigation"
+      aria-label="Navigation principale"
+    >
       <ul className="flex-col-row gap-4 text-white sm:text-start sm:gap-6 sm:pb-0 md:gap-8 lg:gap-10">
         {navElements.map((navElement) => (
           <li
