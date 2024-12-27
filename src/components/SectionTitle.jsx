1dcc76010a941215ca8e39162d9fe5a44619552b
Priscouw <priscouw@gmail.com>
2024-12-27 13:47:34 +0100

    <style> change proptypes

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,6 +1,6 @@
-import { PropTypes } from "prop-types";
+import propTypes from "prop-types";
 
-export const SectionTitle = ({ title, className }) => {
+export const SectionTitle = ({ title = "", className = "" }) => {
   return (
     <h3
       className={`text-purple text-center text-2xl mb-16 sm:text-3xl lg:text-left ${className}`}
@@ -11,6 +11,6 @@ export const SectionTitle = ({ title, className }) => {
 };
 
 SectionTitle.propTypes = {
-  title: PropTypes.string.isRequired,
-  className: PropTypes.string,
+  title: propTypes.string.isRequired,
+  className: propTypes.string,
 };
