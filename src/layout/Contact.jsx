ad58e931c1a655732cbba395234f2d811f07bb4c
Priscouw <priscouw@gmail.com>
2024-12-29 13:19:21 +0100

    <refactor> add .map on array

--- src/layout/Contact.jsx
+++ src/layout/Contact.jsx
@@ -4,33 +4,43 @@ import { CiMail } from "react-icons/ci";
 import { CiLinkedin } from "react-icons/ci";
 import { FiGithub } from "react-icons/fi";
 
+const contactElements = [
+  {
+    title: "github",
+    ariaLabel: "Lien vers mon profil Github",
+    link: "https://github.com/Priscouw",
+    logo: <FiGithub />,
+  },
+  {
+    title: "linkedin",
+    ariaLabel: "Lien vers mon profil Linkedin",
+    link: "https://linkedin.com/in/g-priscilla",
+    logo: <CiLinkedin />,
+  },
+  {
+    title: "email",
+    ariaLabel: "Envoyez un mail à Priscilla",
+    link: "mailto:gu.priscilla7@gmail.com",
+    logo: <CiMail />,
+  },
+];
+
 const Contact = () => {
   return (
     <section className="container-section" id="contact">
       <SectionTitle title="Contact" />
       <div className="text-white flex justify-center gap-7 text-3xl sm:text-4xl sm:gap-10 lg:justify-start ">
-        <a
-          href="https://github.com/Priscouw"
-          aria-label="Lien vers mon profil Github"
-          title="github"
-        >
-          <FiGithub />
-        </a>
-        <a
-          href="https://linkedin.com/in/g-priscilla"
-          aria-label="Lien vers mon profil Linkedin"
-          title="linkedin"
-        >
-          <CiLinkedin />
-        </a>
-
-        <a
-          href="mailto:gu.priscilla7@gmail.com"
-          aria-label="Envoyez un mail à Priscilla"
-          title="email"
-        >
-          <CiMail />
-        </a>
+        {contactElements.map((element) => (
+          <a
+            key={element.title}
+            href={element.link}
+            title={element.title}
+            aria-label={element.ariaLabel}
+            className="transition hover:scale-125 hover:transition hover:ease-linear"
+          >
+            {element.logo}
+          </a>
+        ))}
       </div>
     </section>
   );
